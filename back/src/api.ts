4fc26611336f595b8c284579c0b6488cc7d4b7a5
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-28 16:21:27 +0200

    dev: added typescript

--- back/src/api.ts
+++ back/src/api.ts
@@ -1,8 +1,8 @@
-const express = require("express")
+import express from "express"
 const app = express.Router()
 
 app.get("/date", (req, res) => {
   res.json({ date: new Date() })
 })
 
-module.exports = app
+export default app
