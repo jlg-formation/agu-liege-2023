25f3f8362f99f2430ecf27fa1528ce82305e70e3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-29 16:59:59 +0200

    dev: added reactive forms

--- front/src/app/stock/stock.module.ts
+++ front/src/app/stock/stock.module.ts
@@ -1,13 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { AddComponent } from './add/add.component';
 import { StockRoutingModule } from './stock-routing.module';
 import { StockComponent } from './stock.component';
-import { AddComponent } from './add/add.component';
 
 @NgModule({
   declarations: [StockComponent, AddComponent],
-  imports: [CommonModule, StockRoutingModule, FontAwesomeModule],
+  imports: [
+    CommonModule,
+    StockRoutingModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+  ],
 })
 export class StockModule {}
