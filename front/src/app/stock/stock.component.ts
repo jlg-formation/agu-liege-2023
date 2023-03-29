5e47a23a29d671a47bb7afc5ceea57b86e81d526
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-29 15:50:36 +0200

    dev: added lifecyle method on component

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,17 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import {
   faRotateRight,
   faPlus,
   faTrashAlt,
 } from '@fortawesome/free-solid-svg-icons';
+import { Article } from '../interfaces/article';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
   styleUrls: ['./stock.component.scss'],
 })
-export class StockComponent {
-  faRotateRight = faRotateRight;
+export class StockComponent implements OnDestroy {
+  articles: Article[] = [
+    {
+      id: 'a1',
+      name: 'Tournevis',
+      price: 3.99,
+      qty: 100,
+    },
+    {
+      id: 'a2',
+      name: 'Pelle',
+      price: 5,
+      qty: 54,
+    },
+  ];
   faPlus = faPlus;
+  faRotateRight = faRotateRight;
   faTrashAlt = faTrashAlt;
+
+  constructor() {
+    console.log('new stock component');
+  }
+
+  ngOnDestroy(): void {
+    console.log('stock component disappear');
+  }
 }
