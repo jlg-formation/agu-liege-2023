14260c418ec807a35adc2211b91915eb8289516a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-29 14:46:18 +0200

    dev: added fontawesome

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent {
-
+  faCoffee = faCoffee;
 }
--- front/src/app/stock/stock.module.ts
+++ front/src/app/stock/stock.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { StockRoutingModule } from './stock-routing.module';
 import { StockComponent } from './stock.component';
 import { AddComponent } from './add/add.component';
 
-
 @NgModule({
-  declarations: [
-    StockComponent,
-    AddComponent
-  ],
-  imports: [
-    CommonModule,
-    StockRoutingModule
-  ]
+  declarations: [StockComponent, AddComponent],
+  imports: [CommonModule, StockRoutingModule, FontAwesomeModule],
 })
-export class StockModule { }
+export class StockModule {}
