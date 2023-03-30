a067805a14b70ad251821019df81ed7cce11cd15
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-30 17:12:41 +0200

    dev: added autofocus directive

--- front/src/app/widgets/widgets.module.ts
+++ front/src/app/widgets/widgets.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AsyncIconButtonComponent } from './async-icon-button/async-icon-button.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AutofocusDirective } from './autofocus.directive';
 
 @NgModule({
-  declarations: [AsyncIconButtonComponent],
+  declarations: [AsyncIconButtonComponent, AutofocusDirective],
   imports: [CommonModule, FontAwesomeModule],
-  exports: [AsyncIconButtonComponent],
+  exports: [AsyncIconButtonComponent, AutofocusDirective],
 })
 export class WidgetsModule {}
