7a45635c44de5398c52341b1edefa483ac7d073c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-30 15:44:18 +0200

    dev: added article on back

--- back/src/api.ts
+++ back/src/api.ts
@@ -1,8 +1,28 @@
 import express from "express"
+import { Article } from "./interfaces/article"
 const app = express.Router()
+
+let articles: Article[] = [
+  {
+    id: "a1",
+    name: "Tournevis",
+    price: 3.99,
+    qty: 100,
+  },
+  {
+    id: "a2",
+    name: "Pelle",
+    price: 5,
+    qty: 54,
+  },
+]
 
 app.get("/date", (req, res) => {
   res.json({ date: new Date() })
+})
+
+app.get("/articles", (req, res) => {
+  res.json(articles)
 })
 
 export default app
