65da4e71e6b39c7c3aaf498163476a55eed1cdc4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-30 10:00:27 +0200

    dev: added submit action
    - delay
    - add
    - refresh
    - navigate

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { Article } from '../interfaces/article';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { Article, NewArticle } from '../interfaces/article';
+
+const generateId = () => {
+  return Date.now() + '_' + Math.round(Math.random() * 1e12);
+};
 
 const articles: Article[] = [
   {
@@ -28,5 +32,16 @@ export class ArticleService {
       this.articles$.value.push({ id: 'a3', name: 'Truc', price: 1, qty: 12 });
       this.articles$.next(this.articles$.value);
     }, 2000);
+  }
+
+  add(newArticle: NewArticle): Observable<void> {
+    const article = { ...newArticle, id: generateId() };
+    articles.push(article);
+    return of(undefined);
+  }
+
+  refresh(): Observable<void> {
+    this.articles$.next(articles);
+    return of(void 0);
   }
 }
--- front/src/app/stock/add/add.component.ts
+++ front/src/app/stock/add/add.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { delay, of, switchMap } from 'rxjs';
+import { NewArticle } from 'src/app/interfaces/article';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-add',
@@ -8,8 +12,6 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./add.component.scss'],
 })
 export class AddComponent {
-  faPlus = faPlus;
-
   f = new FormGroup({
     name: new FormControl('xxx', [
       Validators.required,
@@ -18,4 +20,28 @@ export class AddComponent {
     price: new FormControl(0, [Validators.required]),
     qty: new FormControl(0, [Validators.required]),
   });
+  faPlus = faPlus;
+
+  constructor(
+    private readonly articleService: ArticleService,
+    private readonly router: Router,
+    private readonly route: ActivatedRoute
+  ) {}
+
+  submit() {
+    console.log('submit');
+    of(undefined)
+      .pipe(
+        delay(1000),
+        switchMap(() => {
+          const newArticle = this.f.value as NewArticle;
+          return this.articleService.add(newArticle);
+        }),
+        switchMap(() => this.articleService.refresh()),
+        switchMap(() => {
+          return this.router.navigate(['..'], { relativeTo: this.route });
+        })
+      )
+      .subscribe();
+  }
 }
