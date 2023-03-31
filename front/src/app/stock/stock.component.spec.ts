678906aada8e261140028dd7124131f574a8c315
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-31 13:05:28 +0200

    test: full coverage of stock

--- front/src/app/stock/stock.component.spec.ts
+++ front/src/app/stock/stock.component.spec.ts
@@ -1,11 +1,11 @@
 import {
   ComponentFixture,
   fakeAsync,
-  flush,
   TestBed,
   tick,
 } from '@angular/core/testing';
 import { FontAwesomeTestingModule } from '@fortawesome/angular-fontawesome/testing';
+import { a1 } from 'src/test/article.data';
 import { WidgetsModule } from '../widgets/widgets.module';
 
 import { StockComponent } from './stock.component';
@@ -36,6 +36,37 @@ describe('StockComponent', () => {
     tick(2000);
     component.refresh().subscribe();
     tick(2000);
+    expect(component).toBeTruthy();
+  }));
+
+  it('should remove', fakeAsync(() => {
+    fixture = TestBed.createComponent(StockComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    tick(2000);
+    component.select(a1);
+    component.remove().subscribe();
+    tick(2000);
+    expect(component).toBeTruthy();
+  }));
+
+  it('should select and unselect', fakeAsync(() => {
+    fixture = TestBed.createComponent(StockComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    tick(2000);
+    component.select(a1);
+    component.select(a1);
+    expect(component).toBeTruthy();
+  }));
+
+  it('should reset and set the errorMsg', fakeAsync(() => {
+    fixture = TestBed.createComponent(StockComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    tick(2000);
+    component.resetErrorMsg();
+    component.setErrorMsg(new Error('oups'));
     expect(component).toBeTruthy();
   }));
 });
