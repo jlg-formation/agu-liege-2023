3a6249cc3983dc3dc5bbc8d0d3b6ba3a04c483b6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-31 15:58:34 +0200

    dev: added url rewriting

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,7 +1,14 @@
+import { isDevMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+if (!isDevMode()) {
+  console.log = () => {
+    // empty function
+  };
+}
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
