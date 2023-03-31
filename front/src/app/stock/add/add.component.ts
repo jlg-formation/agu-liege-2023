208d1da4a3f8df3db645d4353aba9a62cbed4765
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-31 14:11:07 +0200

    dev: added rewrite on field for integer guiding

--- front/src/app/stock/add/add.component.ts
+++ front/src/app/stock/add/add.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { faPlus, faCircleNotch } from '@fortawesome/free-solid-svg-icons';
-import { catchError, delay, finalize, of, switchMap, tap } from 'rxjs';
+import {
+  catchError,
+  delay,
+  finalize,
+  map,
+  of,
+  pairwise,
+  startWith,
+  switchMap,
+  tap,
+} from 'rxjs';
 import { NewArticle } from 'src/app/interfaces/article';
 import { ArticleService } from 'src/app/services/article.service';
 
@@ -11,15 +21,15 @@ import { ArticleService } from 'src/app/services/article.service';
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.scss'],
 })
-export class AddComponent {
+export class AddComponent implements OnInit {
   errorMsg = '';
   f = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
     price: new FormControl(0, [Validators.required]),
     qty: new FormControl(0, [Validators.required]),
   });
-  faPlus = faPlus;
   faCircleNotch = faCircleNotch;
+  faPlus = faPlus;
   isSubmitting = false;
 
   constructor(
@@ -27,6 +37,57 @@ export class AddComponent {
     private readonly router: Router,
     private readonly route: ActivatedRoute
   ) {}
+
+  ngOnInit(): void {
+    this.f.controls.qty.valueChanges
+      .pipe(
+        startWith(this.f.controls.qty.value),
+        map((v) => {
+          if (v === null) {
+            return null;
+          }
+          return ('' + v) as string;
+        }),
+        pairwise(),
+        map(([previous, current]) => {
+          console.log('current: ', current, typeof current);
+          console.log('previous: ', previous, typeof previous);
+
+          if (current === null) {
+            return null;
+          }
+          if (current.match(/^[0-9]*$/)) {
+            console.log('matched current: ', current);
+            return current;
+          }
+          console.log('not matched');
+
+          console.log('return previous: ', previous);
+          if (previous === null) {
+            return null;
+          }
+          const result = previous.replace(/[^0-9]/g, '');
+          console.log('result: ', result);
+
+          return result;
+        }),
+        map((v) => {
+          console.log('v: ', v, typeof v);
+          if (v === null) {
+            return null;
+          }
+          if (v === '') {
+            return null;
+          }
+          return +v;
+        }),
+        tap((int) => {
+          console.log('int: ', int);
+          this.f.controls.qty.setValue(int, { emitEvent: false });
+        })
+      )
+      .subscribe();
+  }
 
   submit() {
     console.log('submit');
