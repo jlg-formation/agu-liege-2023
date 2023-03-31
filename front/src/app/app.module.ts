a2fd328ef90906f13d644edf7dd25b38cf3a67cc
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-31 09:18:10 +0200

    dev: added currency pipe

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,15 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { HomeComponent } from './routes/home/home.component';
 import { LegalComponent } from './routes/legal/legal.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ArticleService } from './services/article.service';
 import { HttpArticleService } from './services/http-article.service';
+
+import { registerLocaleData } from '@angular/common';
+import BelgianLocale from '@angular/common/locales/fr-BE';
+
+registerLocaleData(BelgianLocale, 'fr-BE');
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, LegalComponent],
