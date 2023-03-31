a87fa010fe0422d4a29a48bd4fd336fa8b45754c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-31 14:33:43 +0200

    test: coverage 100%

--- front/src/app/stock/add/add.component.spec.ts
+++ front/src/app/stock/add/add.component.spec.ts
@@ -1,7 +1,14 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FontAwesomeTestingModule } from '@fortawesome/angular-fontawesome/testing';
+import { throwError } from 'rxjs';
+import { ArticleService } from 'src/app/services/article.service';
 
 import { AddComponent } from './add.component';
 
@@ -9,22 +16,70 @@ describe('AddComponent', () => {
   let component: AddComponent;
   let fixture: ComponentFixture<AddComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        FontAwesomeTestingModule,
-        ReactiveFormsModule,
-      ],
-      declarations: [AddComponent],
-    }).compileComponents();
+  describe('without injection', () => {
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        imports: [
+          RouterTestingModule,
+          FontAwesomeTestingModule,
+          ReactiveFormsModule,
+        ],
+        declarations: [AddComponent],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(AddComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      fixture = TestBed.createComponent(AddComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should write qty', () => {
+      component.f.controls.qty.setValue(null);
+      component.f.controls.qty.setValue(1);
+      component.f.controls.qty.setValue('1.' as unknown as number);
+      component.f.controls.qty.setValue(null);
+      component.f.controls.qty.setValue('.' as unknown as number);
+      component.f.controls.qty.setValue('' as unknown as number);
+
+      expect(component).toBeTruthy();
+    });
+
+    it('should submit', fakeAsync(() => {
+      component.submit();
+      tick(300);
+
+      expect(component).toBeTruthy();
+    }));
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('with injection', () => {
+    let component: AddComponent;
+    let fixture: ComponentFixture<AddComponent>;
+
+    it('should submit in error', fakeAsync(async () => {
+      const myArticleService = new ArticleService();
+      myArticleService.add = () => throwError(() => new Error('oups'));
+      await TestBed.configureTestingModule({
+        imports: [
+          RouterTestingModule,
+          FontAwesomeTestingModule,
+          ReactiveFormsModule,
+        ],
+        providers: [{ provide: ArticleService, useValue: myArticleService }],
+        declarations: [AddComponent],
+      }).compileComponents();
+
+      fixture = TestBed.createComponent(AddComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+
+      component.submit();
+      tick(300);
+
+      expect(component).toBeTruthy();
+    }));
   });
 });
