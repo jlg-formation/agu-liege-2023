64fba50e4983e931c74d420c4ca68362449ad1e1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-31 11:42:39 +0200

    dev: added coverage for refresh

--- front/src/app/services/http-article.service.spec.ts
+++ front/src/app/services/http-article.service.spec.ts
@@ -1,19 +1,59 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { catchError, of } from 'rxjs';
 
-import { HttpArticleService } from './http-article.service';
+import { HttpArticleService, url } from './http-article.service';
 
 describe('HttpArticleService', () => {
   let service: HttpArticleService;
+  let ctrl: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(HttpArticleService);
+    ctrl = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    ctrl.verify();
   });
 
   it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should refresh', () => {
+    service.refresh().subscribe();
+    const req = ctrl.expectOne(url);
+    expect(req.request.method).toEqual('GET');
+    req.flush([]);
+    expect(service).toBeTruthy();
+  });
+
+  it('should refresh in error', (done: DoneFn) => {
+    let mustGoHere = false;
+    service
+      .refresh()
+      .pipe(
+        catchError(() => {
+          mustGoHere = true;
+          return of(undefined);
+        })
+      )
+      .subscribe({
+        complete: () => {
+          expect(mustGoHere).toBe(true);
+          done();
+        },
+      });
+    const req = ctrl.expectOne(url);
+    expect(req.request.method).toEqual('GET');
+    req.flush('', { status: 404, statusText: 'Not Found' });
     expect(service).toBeTruthy();
   });
 });
