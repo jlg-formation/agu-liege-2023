5e0160debe8a7c55060de10fe3028e164866b022
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-31 09:41:48 +0200

    dev: added ellipsis pipe

--- front/src/app/widgets/widgets.module.ts
+++ front/src/app/widgets/widgets.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { AsyncIconButtonComponent } from './async-icon-button/async-icon-button.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AutofocusDirective } from './autofocus.directive';
+import { EllipsisPipe } from './ellipsis.pipe';
 
 @NgModule({
-  declarations: [AsyncIconButtonComponent, AutofocusDirective],
+  declarations: [AsyncIconButtonComponent, AutofocusDirective, EllipsisPipe],
   imports: [CommonModule, FontAwesomeModule],
-  exports: [AsyncIconButtonComponent, AutofocusDirective],
+  exports: [AsyncIconButtonComponent, AutofocusDirective, EllipsisPipe],
 })
 export class WidgetsModule {}
