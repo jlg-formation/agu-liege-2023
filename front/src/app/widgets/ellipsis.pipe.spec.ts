6eb0d548e384e8853ce8f86d1283aebfb0cd2fc8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-31 14:50:57 +0200

    test: coverate pipe ok

--- front/src/app/widgets/ellipsis.pipe.spec.ts
+++ front/src/app/widgets/ellipsis.pipe.spec.ts
@@ -3,6 +3,19 @@ import { EllipsisPipe } from './ellipsis.pipe';
 describe('EllipsisPipe', () => {
   it('create an instance', () => {
     const pipe = new EllipsisPipe();
+    let result = pipe.transform('truc');
+    expect(result).toBe('truc');
+    result = pipe.transform('truc', 3);
+    expect(result).toBe('tru...');
+    result = pipe.transform('01234567890');
+    expect(result).toBe('0123456789...');
+    let mustGoHere = false;
+    try {
+      pipe.transform(12);
+    } catch (err) {
+      mustGoHere = true;
+    }
+    expect(mustGoHere).toBe(true);
     expect(pipe).toBeTruthy();
   });
 });
