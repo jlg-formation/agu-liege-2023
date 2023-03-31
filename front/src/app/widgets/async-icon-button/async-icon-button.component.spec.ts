dbcb49c1e98d10899e3a48809956ede92cac52de
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-31 15:21:35 +0200

    test: async button

--- front/src/app/widgets/async-icon-button/async-icon-button.component.spec.ts
+++ front/src/app/widgets/async-icon-button/async-icon-button.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FontAwesomeTestingModule } from '@fortawesome/angular-fontawesome/testing';
+import { throwError } from 'rxjs';
 
 import { AsyncIconButtonComponent } from './async-icon-button.component';
 
@@ -19,6 +20,18 @@ describe('AsyncIconButtonComponent', () => {
   });
 
   it('should create', () => {
+    component.observable();
+    expect(component).toBeTruthy();
+  });
+  it('should run doSomething', () => {
+    component.doSomething();
+    expect(component).toBeTruthy();
+  });
+  it('should run doSomething in error', () => {
+    component.observable = () => throwError(() => new Error('oups'));
+    component.doSomething();
+    component.observable = () => throwError(() => 'oups');
+    component.doSomething();
     expect(component).toBeTruthy();
   });
 });
